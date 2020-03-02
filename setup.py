cf1f266f323c2a4d57c2563371346c1df4060d71
gpdas <contac@gauthampdas.com>
2020-03-02 15:41:50 +0000

    fixes

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['cbpae_turtlebot3'],
+    packages=['cbpae'],
     package_dir={'': 'src'},
 )
 
